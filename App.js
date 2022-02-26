2dd271b816fd0fe6dfd78af2bbee9aeb8e7a26de
shweta-stack <itsshweta2812@gmail.com>
2022-02-26 15:44:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+// import './App.css';
+import Header from './Components/Header';
+import LinkShortener from './Components/LinkShortener';
+import Footer from './Components/Footer';
+import Stats from './Components/Stats';
+
+function App() {
+  return (
+    <div className="App">
+      < Header />
+      < LinkShortener />
+      <Footer/>
+      <Stats />
+    </div>
+  );
+}
+
+export default App;
